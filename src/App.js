db27a4dcc46b2ffc38f9437ab66922e3a2c7f389
MEVEN <meven.ameline@gmail.com>
2022-10-16 15:20:31 +0200

    Installation ESlint + firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { db } from './firebase-config'
+import { collection, getDocs } from 'firebase/firestore'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount () {
+    console.log('componentDidMount')
+    getDocs(collection(db, 'players'))
+      .then(res => {
+        res.forEach(player => {
+          console.log('player', player.id, player.data())
+        })
+      })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  };
 }
 
-export default App;
+export default App
---
+++ src/firebase-config.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+
+import { getFirestore } from 'firebase/firestore'
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: 'react-firebase-c1956.firebaseapp.com',
+  projectId: 'react-firebase-c1956',
+  storageBucket: 'react-firebase-c1956.appspot.com',
+  messagingSenderId: '1056437714977',
+  appId: '1:1056437714977:web:2cb3def48663bdc7e3a360',
+  measurementId: 'G-9CYDTH624H'
+}
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig)
+
+export const db = getFirestore(app)
